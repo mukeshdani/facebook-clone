d72ae58f930d7abf429c582dd5b9ba2da1b77c79
mukeshdani <mukeshdani00@gmail.com>
2021-09-23 13:33:24 +0530

    update app components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { GetWallData, GetUserData } from "../services/UserData";
+import Header from "./Bootstrap/Header";
+import Feed from "./Feed";
+import Main from "./Main";
+import Sidebar from "./Sidebar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    UserData: null,
+    WallData: []
+  };
+  componentDidMount() {
+    GetUserData().then(({ data: UserData }) => this.setState({ UserData }));
+    GetWallData().then(({ data: WallData }) => this.setState({ WallData }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header
+          className="Header"
+          UserMeta={this.state.UserData && this.state.UserData.UserMeta}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Facebook Clone
+        </Header>
+        {this.state.UserData ? (
+          <div className="container">
+            <div className="row">
+              <Sidebar
+                ContentList={this.state.UserData.ContentList}
+                UserMeta={this.state.UserData.UserMeta}
+              />
+              <Main WallData={this.state.WallData} />
+              <Feed
+                Birthdays={this.state.UserData.Birthdays}
+                Pages={this.state.UserData.Pages}
+                NewsFeeds={this.state.UserData.NewsFeeds}
+              />
+            </div>
+          </div>
+        ) : (
+          "Login Screen"
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
